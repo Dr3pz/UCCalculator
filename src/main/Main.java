fc15e99e77a9fccc9e0fea3bf6887d74f0dcab0d
Sovun89 <savvawar2011@gmail.com>
2026-08-25 17:57:50 +0500

    1.0 commit | new REB version

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,168 +1,89 @@
 package main;
 
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.geometry.Insets;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
-import static javafx.geometry.Pos.TOP_CENTER;
 
 public class Main extends Application {
 
     public void start (Stage stage){
 
+        // Window size --->
+        stage.setWidth(400);
+        stage.setHeight(600);
+        stage.setResizable(false);
+
+        // Add node --->
+
+        // Add TextField (InputField)
         int maxLengthInputField = 32;
 
-        // Size Window --->
-        stage.setMaxWidth(600);
-        stage.setMaxHeight(800);
-        stage.setMinWidth(600);
-        stage.setMinHeight(800);
-
-        stage.setWidth(600);
-        stage.setHeight(800);
-
-        // Test typing label --->
-        HBox labelHBox = new HBox();
-
-        Label typeLabel = new Label("CHAR: PRESS");
-
-        Label lengthLabel = new Label("L: HEX");
-
-        Region spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-
-        labelHBox.getChildren().addAll(typeLabel, spacer, lengthLabel);
-
-        // inputLine --->
         TextField inputField = new TextField();
+        inputField.setPrefSize(Double.MAX_VALUE, 75);
 
-        inputField.setMaxSize(stage.getWidth(), 75);
-        inputField.setMinSize(stage.getWidth(), 75);
-
-        inputField.setStyle("-fx-font-size: 28; -fx-background-color: #10241b; -fx-text-fill: white;");
-
-        inputField.textProperty().addListener((observable, oldValue, newValue) -> {
-            lengthLabel.setText(String.format("L: %s", newValue.length()));
-            if (newValue.length() > maxLengthInputField) {
-                inputField.setText(oldValue);
-            }
-        });
-
-        // Buttons --->
+        // Add Buttons
         Button plusButton = new Button("+");
-        plusButton.setStyle("-fx-font-size: 48; -fx-background-color: #dfd5c6; -fx-text-fill: #2c3531;");
-        plusButton.setMaxSize(100, 100);
-        plusButton.setMinSize(100, 100);
-
         Button minusButton = new Button("-");
-        minusButton.setStyle("-fx-font-size: 48; -fx-background-color: #dfd5c6; -fx-text-fill: #2c3531;");
-        minusButton.setMaxSize(100, 100);
-        minusButton.setMinSize(100, 100);
-
         Button multiplyButton = new Button("*");
-        multiplyButton.setStyle("-fx-font-size: 48; -fx-background-color: #dfd5c6; -fx-text-fill: #2c3531;");
-        multiplyButton.setMaxSize(100, 100);
-        multiplyButton.setMinSize(100, 100);
-
         Button divideButton = new Button("/");
-        divideButton.setStyle("-fx-font-size: 48; -fx-background-color: #dfd5c6; -fx-text-fill: #2c3531;");
-        divideButton.setMaxSize(100, 100);
-        divideButton.setMinSize(100, 100);
 
-        Button clearButton = new Button();
-        clearButton.setText("Clear");
-
-        clearButton.setStyle("-fx-font-size: 48; -fx-background-color: #d97a53; -fx-text-fill: #2c3531;");
-        clearButton.setMaxSize(225, 100);
-        clearButton.setMinSize(225, 100);
-
-        // GridPane --->
+        // Add GridPane --->
         GridPane gridPane = new GridPane();
 
-        // BorderPane --->
-        BorderPane borderPane = new BorderPane();
+        gridPane.add(plusButton,0,0);
+        plusButton.setPrefSize(50, 50);
 
-        // Root settings --->
-        Scene scene = new Scene(borderPane);
+        gridPane.add(minusButton,1,0);
+        minusButton.setPrefSize(50, 50);
 
-        // Other Settings --->
+        gridPane.add(multiplyButton,2,0);
+        multiplyButton.setPrefSize(50, 50);
 
-        stage.setTitle("FXCalculate");
-        scene.getRoot().setStyle("-fx-background-color: #1a3a2b;"); // Background color
+        gridPane.add(divideButton,3,0);
+        divideButton.setPrefSize(50, 50);
 
-        // GridGeo node --->
         gridPane.setHgap(25);
         gridPane.setVgap(25);
 
-        gridPane.setAlignment(TOP_CENTER);
+        gridPane.setAlignment(Pos.TOP_CENTER);
 
-        gridPane.add(plusButton,0,0);
-        gridPane.add(minusButton,1,0);
-        gridPane.add(multiplyButton,2,0);
-        gridPane.add(divideButton,3,0);
-        gridPane.add(clearButton,1,1,2,1);
-
-        // BroderGeo node --->
-        borderPane.setCenter(gridPane);
+        // Add BorderPane --->
+        BorderPane borderPane = new BorderPane();
 
         borderPane.setTop(inputField);
-        BorderPane.setMargin(inputField, new Insets(0,0,75,0));
-        BorderPane.setAlignment(inputField, Pos.TOP_LEFT);
+        BorderPane.setMargin(inputField, new Insets(0, 0, 75, 0));
 
-        borderPane.setBottom(labelHBox);
-        BorderPane.setAlignment(labelHBox, Pos.BOTTOM_CENTER);
+        borderPane.setCenter(gridPane);
 
-        typeLabel.setStyle("-fx-text-size: 12; -fx-text-fill: white;");
+        // Add scene --->
+        Scene scene = new Scene(borderPane);
 
-        lengthLabel.setStyle("-fx-text-size: 12; -fx-text-fill: white; -fx-translate-x: -15;");
+        scene.getRoot().setStyle("-fx-background-color: #017200");
 
-        // Key init --->
-        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
-            typeLabel.setText(String.format("CHAR: %s", keyEvent.getCode()));
-
-            switch (keyEvent.getCode()) {
+        // Key reader/parser --->
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
                 case ENTER:
-                    inputField.clear();
-                    String result = "TEST VALUE";
-                    inputField.insertText(inputField.getCaretPosition(), result);
-                    keyEvent.consume();
-                    break;
-                case ESCAPE:
-                    if (keyEvent.isShiftDown() || keyEvent.isControlDown()) {
-                        Platform.exit();
-                    }
-                    break;
-                case TAB:
-                    keyEvent.consume();
+                    inputField.setText("");
                     break;
             }
         });
 
-        // Button logic --->
-        ButtonOperation(plusButton, inputField, "+");
-        ButtonOperation(minusButton, inputField, "-");
-        ButtonOperation(multiplyButton, inputField, "*");
-        ButtonOperation(divideButton, inputField, "/");
+        // Window init --->
+        String currentCommitVersion = "1.0 REB";
+        stage.setTitle(String.format("FXCalculator v:%s", currentCommitVersion));
 
-        clearButton.setOnAction(event -> {
-            inputField.clear();
-            inputField.requestFocus();
-        });
-
-        // Compiling --->
         stage.setScene(scene);
-
         stage.show();
+
     }
 
     private void ButtonOperation(Button currentButton, TextField inputField, String insertSymbol) {
